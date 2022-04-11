3cf4eebe1858bba88f25ca54b915a5c49c568d99
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-04-11 10:02:09 +0200

    implemented PasswordUtilities

--- API/src/Utilities/PasswordUtlilities.php
+++ API/src/Utilities/PasswordUtlilities.php
@@ -8,3 +8,22 @@
 declare(strict_types=1);
 
 namespace BenSauer\CaseStudySkygateApi\Utilities;
+
+use BenSauer\CaseStudySkygateApi\Utilities\Interfaces\PasswordUtilitiesInterface;
+use RuntimeException;
+
+class PasswordUtilities implements PasswordUtilitiesInterface
+{
+    public function hashPassword(string $pass): string
+    {
+        $ret = password_hash($pass, PASSWORD_BCRYPT);
+        if (is_null($ret)) throw new RuntimeException("The password hash algorithm is invalid");
+        if ($ret === false) throw new RuntimeException("The password hash fails");
+        return $ret;
+    }
+
+    public function checkPassword(string $pass, string $hashedPassword): bool
+    {
+        return password_verify($pass, $hashedPassword);
+    }
+}
