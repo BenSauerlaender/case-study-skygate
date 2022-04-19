5e63546af63a781dd86ff0b971a5b34f3939de51
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-04-19 08:49:43 +0200

    added an assertChangedRowsEqual method

--- API/tests/UnitTests/DatabaseUtilities/Accessors/BaseMySqlAccessorTest.php
+++ API/tests/UnitTests/DatabaseUtilities/Accessors/BaseMySqlAccessorTest.php
@@ -21,6 +21,13 @@ use PHPUnit\Framework\TestCase;
 abstract class BaseMySqlAccessorTest extends BaseDatabaseTest
 {
     /**
+     * The UNIX timestamp at the start of observation
+     *
+     * @var int|null
+     */
+    private static ?int $start = null;
+
+    /**
      * Deletes and re-creates the database and tables
      */
     protected static function resetDB(): void
@@ -34,18 +41,45 @@ abstract class BaseMySqlAccessorTest extends BaseDatabaseTest
     }
 
     /**
+     * SetUps the assertChangedRowsEquals function
+     */
+    protected function startChangedRowsObservation(): void
+    {
+        //save the timestamp
+        self::$start = time();
+
+        //TODO change this:
+        sleep(1);
+    }
+
+    /**
      * Counts changed rows and performs an "assertEquals" on the expected and actual result
      * 
      * @param int $expected The expected number of changed rows
      */
     protected function assertChangedRowsEquals(int $expected): void
     {
+        //all the tables to observe
+        $tables = ["user", "role", "emailChangeRequest"];
 
-        self::$pdo->exec('
-            //TODO
-        ');
+        $changedRows = 0;
 
-        $changedRows;
+        //going over each table
+        foreach ($tables as $table) {
+
+            $stmt = self::$pdo->query(
+                'SELECT updated_at FROM ' . $table . ';'
+            );
+
+            //going over each row
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+                //if the row was updated after the observation start
+                if (strtotime($row["updated_at"]) !== self::$start) {
+                    $changedRows++;
+                }
+            }
+        }
 
         $this->assertEquals($expected, $changedRows);
     }
