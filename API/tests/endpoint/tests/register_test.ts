823f552d731f22c8d1844ef2f2d97b2e9b4bfc3b
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-04-26 10:43:01 +0200

    change testing framework

--- API/tests/endpoint/tests/register_test.ts
+++
@@ -1,7 +0,0 @@
-import got from 'got';
-
-Feature("register as new user");
-
-Scenario("Get store order by id", async({I}) => {
-    const resp = await got<any>('${process.env.BASE_URL}/v1/register')
-});
