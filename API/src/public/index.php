1a79ea8e041649f7d27dac0f88570446e6745ed3
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-04-27 18:22:10 +0200

    outline api routing

--- API/src/public/index.php
+++ API/src/public/index.php
@@ -3,21 +3,37 @@
 //activate strict mode
 declare(strict_types=1);
 
-use BenSauer\CaseStudySkygateApi\DatabaseUtilities\Controller\MySqlConnector;
+try{
+    //load composer dependencies
+    require './../../vendor/autoload.php';
 
-//load composer dependencies
-require './../../vendor/autoload.php';
+    //load dotenv variables from '.env'
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../../", ".env");
+    $dotenv->load();
+
+    $path = $_SERVER["REQUEST_URI"];
+    $method = $_SERVER["REQUEST_METHOD"];
+    $params = $_SERVER["QUERY_STRING"];
+    $headers = //acceptence
+
+    if(isNot /API/v1/)
+    $response = new NotFoundResponse
+    else if(no https)
+    $response = new NotsecureResponse
+    else {
+        $handler = handlerfactory($path)
+
+        $request = new Request(....)
+        $response = $handler->handle($request);
+
+    }
+
+    $response->send();
+    exit();
 
 
-//load dotenv variables from '.env'
-$dotenv = Dotenv\Dotenv::createImmutable("./../../", ".env");
-$dotenv->load();
-
-//connect to the Database
-MySqlConnector::getConnection();
-
-//create database tables if they not exists
-//DatabaseCreator::create(); //TODO remove later
-
-//seed database with roles and admin
-//DatabaseSeeder::seed(["roles","admin"]); //TODO remove later
+}catch(Exception $e){
+    error_log($e);
+    header("Internal Error");
+    exit();
+}
