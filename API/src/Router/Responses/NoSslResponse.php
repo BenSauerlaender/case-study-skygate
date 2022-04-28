18f7e4c6674fb400c0dc2fc5c69afc2c0677e77e
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-04-28 11:39:34 +0200

    outline responses

---
+++ API/src/Router/Responses/NoSslResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Ben Sauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
+ */
+
+//activate strict mode
+declare(strict_types=1);
+
+namespace BenSauer\CaseStudySkygateApi\Router\Responses;
+
+class NoSslResponse extends Response
+{
+}
---
+++ API/src/Router/Responses/Response.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author Ben Sauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
+ */
+
+//activate strict mode
+declare(strict_types=1);
+
+namespace BenSauer\CaseStudySkygateApi\Router\Responses;
+
+use BenSauer\CaseStudySkygateApi\Router\ResponseInterface;
+
+class Response implements ResponseInterface
+{
+}
