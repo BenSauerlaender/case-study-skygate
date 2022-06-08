a5933787fa82d10c6947b5943b99b795fa4f79ae
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-06-08 14:25:53 +0200

    adjust seeds

--- sql/seeds/3roles.sql
+++ sql/seeds/3roles.sql
@@ -1,6 +1,6 @@
 INSERT INTO role
     (name, permissions)
 VALUES 
-    ("user","user:{all}:{userID}"),
+    ("user","user:{all}:{userID};user:read:{all}"),
     ("admin","user:{all}:{all}"),
     ("test","");
