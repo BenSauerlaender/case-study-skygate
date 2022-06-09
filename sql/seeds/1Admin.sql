ef667c62c54cfce5dbe28a2c54aa0cf340039a00
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-06-09 14:43:11 +0200

    fix comments

--- sql/seeds/1Admin.sql
+++ sql/seeds/1Admin.sql
@@ -1,5 +1,5 @@
 INSERT INTO user
     (email, name, postcode, city, phone, hashed_pass, verified, role_id)
 VALUES 
-    ("admin@mail.de","admin1","00000","admintown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,2); --password: 'Password111'
+    ("admin@mail.de","admin1","00000","admintown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,2); /*password: 'Password111'*/
 
--- sql/seeds/2Users.sql
+++ sql/seeds/2Users.sql
@@ -1,5 +1,5 @@
 INSERT INTO user
     (email, name, postcode, city, phone, hashed_pass, verified, role_id)
 VALUES 
-    ("user1@mail.de","user1","00000","usertown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,1), --password: 'Password111'
-    ("user2@mail.de","user2","00000","usertown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,1); --password: 'Password111'
+    ("user1@mail.de","user1","00000","usertown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,1), /*password: 'Password111' */
+    ("user2@mail.de","user2","00000","usertown","015937839","$2y$10$VjPCru9uA3vmhyxUrYxWkOf7feFszPmFDx0sUYx2rmX3BnTfT7YFC",true,1); /*password: 'Password111' */
