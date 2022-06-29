4d984b89970adff732c5b6de199dad7f20968840
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-06-29 16:31:50 +0200

    delete duplicate permission

--- sql/seeds/3roles.sql
+++ sql/seeds/3roles.sql
@@ -2,5 +2,5 @@ INSERT INTO role
     (name, permissions)
 VALUES 
     ("user","getSelf getOwnContactData deleteSelf changeOwnPassword changeOwnEmail logoutSelf getAllUsers"),
-    ("admin","getAllUsers getAllUsersContactData deleteAllUsers changeOwnPassword changeOwnEmail logoutSelf getAllUsers"),
+    ("admin","getAllUsersContactData deleteAllUsers changeOwnPassword changeOwnEmail logoutSelf getAllUsers"),
     ("test","");
